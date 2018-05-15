c6d1cb858bd3ffa47db597a9e57b123431f2a334
Jaired Jawed <jaireddjawed.7@gmail.com>
2018-05-14 19:29:43 -0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -54,71 +54,217 @@ const double prices[5][3][4] = {
 
 double determineBaseCakePrice(string, string, string);
 int getItemIndexFromArray(string, string[], int);
+void swap(int *, int *);
+void sortAllItemsByPrice();
 void displayAndPrintReceipt(string);
 
 int main() {
     bool repeatOrderProcess = true;
     string customerName;
-
+    
     cout << "Welcome to the Conjoined Triangles of Success Bakery!" << endl;
     cout << "Please enter your full name: ";
     cin >> customerName;
-
+    
     while (repeatOrderProcess) {
         string cakeType;
         int cakeTypeLength = sizeof(cakeTypes)/sizeof(cakeTypes[0]);
         cout << "Please choose a cake type. Here are our available ones." << endl;
-
+        
         for (int i = 0; i < cakeTypeLength; i++) {
             cout << i + 1 << ") " << cakeTypes[i] << endl;
         }
-
+        
         cout << "Please enter a cake type: ";
-        cin >> cakeType;
-
+        cout << "Please enter a cake type: ";
+        char cakeChoice;
+        cin >> cakeChoice;
+        
+        bool flag = true;
+        while (flag)
+        {
+            switch(cakeChoice)
+            {
+                case '1':
+                {
+                    cakeType = "Chocolate";
+                    flag = false;
+                }
+                    break;
+                case '2':
+                {
+                    cakeType = "Carrot";
+                    flag = false;
+                }
+                    break;
+                case '3':
+                {
+                    cakeType = "Yellow";
+                    flag = false;
+                }
+                    break;
+                case '4':
+                {
+                    cakeType = "Oreo";
+                    flag = false;
+                }
+                    break;
+                case '5':
+                {
+                    cakeType = "Strawberry";
+                    flag = false;
+                }
+                    break;
+                default:
+                {
+                    cout << "Enter 1 through 5 to choose: ";
+                    cin >> cakeChoice;
+                }
+                    
+            }
+        }
         string frostingType;
         int frostingTypeLength = sizeof(frostingTypes)/sizeof(frostingTypes[0]);
         cout << "Please choose a frosting type. Here are our available ones." << endl;
-
+        
         for (int i = 0; i < frostingTypeLength; i++) {
             cout << i + 1 << ") " << frostingTypes[i] << endl;
         }
-
+        
         cout << "Please enter a frosting type: ";
-        cin >> frostingType;
-
+        char frostingChoice;
+        
+        cin >> frostingChoice;
+        
+        flag = true;
+        while (flag)
+        {
+            
+            switch(frostingChoice)
+            {
+                case '1':
+                {
+                    frostingType = "Chocolate";
+                    flag = false;
+                }
+                    break;
+                case '2':
+                {
+                    frostingType = "Vanilla";
+                    flag = false;
+                }
+                    break;
+                case '3':
+                {
+                    frostingType = "Cream Cheese";
+                    flag = false;
+                }
+                    break;
+                default:
+                {
+                    cout << "Enter 1 through 3 to choose: ";
+                    cin >> frostingChoice;
+                }
+                    
+            }
+        }
+        
         string fillingType;
         int fillingTypeLength = sizeof(fillingTypes)/sizeof(fillingTypes[0]);
         cout << "Please choose a filling type. Here are our available ones." << endl;
-
+        
         for (int i = 0; i < fillingTypeLength; i++) {
             cout << i + 1 << ") " << fillingTypes[i] << endl;
         }
-
-        cout << "Please enter a filling type: ";
-        cin >> fillingType;
-
+        
+        char fillingChoice;
+        cin >> fillingChoice;
+        flag = true;
+        while (flag)
+        {
+            
+            switch(fillingChoice)
+            {
+                case '1':
+                {
+                    fillingType = "Fudge";
+                    flag = false;
+                }
+                    break;
+                case '2':
+                {
+                    fillingType = "Pudding";
+                    flag = false;
+                }
+                    break;
+                case '3':
+                {
+                    fillingType = "Custard";
+                    flag = false;
+                }
+                    break;
+                case '4':
+                {
+                    fillingType = "Mousse";
+                    flag = false;
+                }
+                    break;
+                default:
+                {
+                    cout << "Enter 1 through 4 to choose: ";
+                    cin >> fillingChoice;
+                }
+                    
+            }
+        }
+        
         double price = determineBaseCakePrice(cakeType, frostingType, fillingType);
         cout << price << endl;
-
+        
         // add the new item to the user's cart
         userCakeTypes.push_back(cakeType);
         userFrostingTypes.push_back(frostingType);
         userFillingTypes.push_back(fillingType);
         cart.push_back(price);
-
-        string repeatOrder;
-        cout << "Would you like to make another order? (Yes (Y) / No (N) ): " << endl;
+        
+        char repeatOrder;
+        cout << "Would you like to make another order? (Yes (y) / No (n)): " << endl;
         cin >> repeatOrder;
         
-        // if the user doesn't want to order again, print the receipt and end the program, otherwise repeat
-        if (repeatOrder == "no") {
-            repeatOrderProcess = false;
-            displayAndPrintReceipt(customerName);
-            cout << "Thanks " << customerName << ", your order will be completed shortly." << endl;
+        flag = true;
+        while(flag)
+        {
+            switch (repeatOrder)
+            {
+                case 'y':
+                {
+                    repeatOrderProcess = true;
+                    flag = false;
+                }
+                    break;
+                case 'n':
+                {
+                    // if the user doesn't want to order again, print the receipt and end the program, otherwise repeat
+                    repeatOrderProcess = false;
+                    flag = false;
+                    cout << "Thanks " << customerName << ", your order will be completed shortly." << endl;
+                    displayAndPrintReceipt(customerName);
+                }
+                    break;
+                    
+                default:
+                {
+                    cout << "Enter y or n to choose: ";
+                    cin >> repeatOrder;
+                }
+            }
         }
+        
+        
+        
     }
-
+    
+    
     return 0;
 }
 
@@ -127,11 +273,11 @@ double determineBaseCakePrice(string cakeType, string frostingType, string filli
     int cakeTypeLength = sizeof(cakeTypes)/sizeof(cakeTypes[0]);
     int frostingTypeLength = sizeof(frostingTypes)/sizeof(frostingTypes[0]);
     int fillingTypeLength = sizeof(fillingTypes)/sizeof(fillingTypes[0]);
-
+    
     int cakeTypeIndex = getItemIndexFromArray(cakeType, cakeTypes, cakeTypeLength);
     int frostingTypeIndex = getItemIndexFromArray(frostingType, frostingTypes, frostingTypeLength);
     int fillingTypeIndex = getItemIndexFromArray(fillingType, fillingTypes, fillingTypeLength);
-
+    
     // loop through the prices array, if all indexes match then return a price
     for (int cake = 0; cake < cakeTypeLength; cake++) {
         for (int frosting = 0; frosting < frostingTypeLength; frosting++) {
@@ -142,7 +288,7 @@ double determineBaseCakePrice(string cakeType, string frostingType, string filli
             }
         }
     }
-
+    
     return 0.00;
 }
 
@@ -153,26 +299,52 @@ int getItemIndexFromArray(string itemName, string items[], int itemLength) {
             return i;
         }
     }
-
+    
     return -1;
+}
+
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void sortAllItemsByPrice() {
+    int min_idx;
+
+    for (int i = 0; i < cart.size() - 1; i++) {
+        min_idx = i;
+        for (int j = i + 1; i < cart.size(); j++) {
+            if (cart.at(j) < cart.at(min_idx)) {
+                min_idx = j;
+            }
+        }
+        
+        double temp = cart.at(min_idx);
+        cart.at(min_idx) = cart.at(i);
+        cart.at(i) = temp;
+    }
 }
 
 // saves a receipt to a text file based on customer information
 void displayAndPrintReceipt(string customerName) {
+    sortAllItemsByPrice();
 
     ofstream writeReceiptFile;
     writeReceiptFile.open("receipt.txt", fstream::app);
-
+    writeReceiptFile << "Order #" << rand() << endl;
+    writeReceiptFile << "________________________________" << endl;
+    writeReceiptFile << "Customer Name: " << customerName << endl;
+    
     // loop through vector, print receipt, and save it to the file
     for ( int i = 0; i < cart.size(); i++) {
-        writeReceiptFile << "Order #" << rand() << endl;
-        writeReceiptFile << "________________________________" << endl;
-        writeReceiptFile << "Customer Name: " << customerName << endl;
+        
+        
         writeReceiptFile << "Cake Type: " << userCakeTypes.at(i) << endl;
         writeReceiptFile << "Cake Frosting: " << userFrostingTypes.at(i) << endl;
         writeReceiptFile << "Cake Filling: " << userFillingTypes.at(i) << endl;
-        writeReceiptFile << "Total: " << cart.at(i) << endl;
+        writeReceiptFile << "Price: " << cart.at(i) << endl;
     }
-
+    
     writeReceiptFile.close();
 }
